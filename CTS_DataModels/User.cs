683e0b39d5e636a7937c62ed15da741a9eaf816d
brianbusby21 <armysgt97@gmail.com>
2019-08-18 13:41:34 -0500

    Initial Commit

---
+++ CTS_DataModels/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTS_DataModels
+{
+    class User
+    {
+        private ChoreList _assignedChoreList;
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public short Age { get; set; }
+
+        public string Role { get; set; }
+
+        public User(string name)
+        {
+            this.Name = name;
+            _assignedChoreList = new ChoreList();
+        }
+
+        public void AssignChore(Chore newChore)
+        {
+            if (newChore != null)
+            {
+                _assignedChoreList.AddChore(newChore);
+            }
+        }
+
+        public void UpdateChore()
+        {
+
+        }
+
+        public void UnassignChore(string choreName)
+        {
+            _assignedChoreList.DeleteChore(choreName);
+
+        }
+
+    }
+}
---
+++ CTS_UserInterface/MainUserInterface.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CTS_UserInterface
+{
+    /// <summary>
+    /// Interaction logic for MainUserInterface.xaml
+    /// </summary>
+    public partial class MainUserInterface : Window
+    {
+        private string _selectedUser;
+        public MainUserInterface(string user)
+        {
+            InitializeComponent();
+            this._selectedUser = user;
+        }
+    }
+}
