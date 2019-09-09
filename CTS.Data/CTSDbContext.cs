db36e13f67ebc75f30c275706a3ea0297e379896
brianbusby21 <armysgt97@gmail.com>
2019-09-08 19:19:38 -0500

    Installed Entity Framework and enabled Code First Migrations

---
+++ CTS.Data/CTSDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTS.Data
+{
+    public class CTSDbContext : DbContext
+    {
+        public DbSet<Child> Children { get; set; }
+
+        public DbSet<Chore> Chores { get; set; }
+
+        public CTSDbContext()
+            :base("name=DefaultConnection")
+        {
+                
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Table names match entity names by default (don't pluralize)
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            // Globally disable the convention for cascading deletes
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            modelBuilder.Entity<Child>()
+                        .Property(c => c.Id) // Client must set the ID.
+                        .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+        }
+
+    }
+}
